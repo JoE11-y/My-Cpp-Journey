37defee85af39b1b1cf8b8af898aba3b2e9a0272
JoE11-y <blockydevjoe@gmail.com>
2022-06-28 22:33:11 +0100

    init

---
+++ Intro-C++/hello.cpp
@@ -0,0 +1,7 @@
+#include <iostream>
+
+int main() {
+    std::cout << "Hello world, I'm ready to play with c++";
+    return 0;
+}
+
---
+++ Intro-C++/usingNamespace.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    cout << "Tying std:: can be a drag sometimes";
+    cout << "Namespace helps out";
+    cout << "But be careful and ensure to be explicit";
+    cout << "As in larger project it may refer to another cmd in a diff library";
+    return 0;
+}
